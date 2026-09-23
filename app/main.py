20b290e5ec648a9fd4b382ac9bcf99bb3f617c87
Mateusz Sobolak <mateusz135755@wp.com>
2026-09-23 13:32:41 +0200

    Replaced direct indexing with dict.get()

--- app/main.py
+++ app/main.py
@@ -9,14 +9,14 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    [Person(person["name"], person["age"]) for person in people]
+    [Person(person.get("name"), person.get("age")) for person in people]
 
     all_people = Person.people
     for person in people:
         if person.get("wife") is not None:
-            all_people[person["name"]].wife = all_people[person["wife"]]
+            all_people[person.get("name")].wife = all_people[person.get("wife")]
 
         if person.get("husband") is not None:
-            all_people[person["name"]].husband = all_people[person["husband"]]
+            all_people[person.get("name")].husband = all_people[person.get("husband")]
 
     return list(Person.people.values())
